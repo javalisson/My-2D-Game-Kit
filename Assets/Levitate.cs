d08ba016c9a67da512a7825a799a352c3c0a7a27
Alisson R S Prestes <javalisson@gmail.com>
2020-01-26 05:02:43 -0300

    Lots of changes ¯\_(ツ)_/¯
    Fixed Moving Platforms error and added new scenes

---
+++ Assets/Levitate.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levitate : MonoBehaviour
+{
+    public float speed = 1f;
+    Vector2 velocity;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        // transform.position += new Vector3(0f, speed, 0f);
+        rb.MovePosition(rb.position + velocity);
+    }
+
+    public void MoveLeft()
+    {
+        Debug.Log("Move left");
+        velocity = new Vector2(-speed, 0.1f);
+    }
+
+    public void MoveRight()
+    {
+        Debug.Log("Move right");
+        velocity = new Vector2(speed, 0.1f);
+    }
+}
